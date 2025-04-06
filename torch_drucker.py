315de642ad29bfdbaa123b8569f77297f213e661
Alexey Snytnikov <snytav@gmail.com>
2025-04-06 12:06:19 +0200

    timestep in a file

--- torch_drucker.py
+++ torch_drucker.py
@@ -21,70 +21,71 @@ def roll(tensor, shift, axis):
     after = tensor.narrow(axis, after_start, shift)
     return torch.cat([after, before], axis)
 
-def timestep(x,v,f_in,T,N,M,dt,dx,dv):
-    f = torch.clone(f_in)
-    # Plot EDF f(x,v,t) in phase space
-    if T % 100 == -1:
-        plt.pcolormesh(x[1:-1], v[1:-1], f[1:-1, 1:-1].T, shading='auto', cmap='hot')
-        plt.axis('square')
-        plt.clim(0, 0.0001)
-        plt.colorbar()
-        plt.title(f'Nonlinear Landau damping, t = {T * dt:.2f}', fontsize=18)
-        plt.draw()
-        plt.pause(0.01)
-
-    # X-coordinate shift at half time step
-    for J in range(1, M - 1):
-        SGN = torch.sign(v[J])
-        SHIFT = torch.abs(v[J]) * 0.5 * dt / dx
-        II = int(SHIFT)
-        SHIFT -= II
-
-        # Make circular shift
-        f_temp = roll(f[:, J], SGN.int() * II, 0)
-        I = torch.arange(1, N - 1).to(f_temp.device)
-        Dxf = torch.zeros(N).to(f_temp.device)
-        Dxf[1:-1] = SHIFT * (f_temp[1:-1] + SGN * (f_temp[2:] - f_temp[:-2]) * (1.0 - SHIFT) / 4.0)
-
-        # Apply periodic border conditions for Dxf
-        Dxf[-1] = Dxf[1]
-        Dxf[0] = Dxf[-2]
-
-        # New distribution function after shift
-        f[1:-1, J] = f_temp[1:-1] + Dxf[I - SGN.int()] - Dxf[I]
-
-    # Apply periodic boundaries in X-coordinate
-    f[-1, :] = f[1, :]
-    f[0, :] = f[-2, :]
-
-    # Electrical field strength from exact solution of Poisson's equation
-    rho = torch.trapz(f, v, axis=1)
-    E1 = torch.cumulative_trapezoid(rho, x)
-    E1 = torch.cat((torch.zeros(1).to(E1.device), E1), dim=0)
-    E = E1 - x
-    E -= torch.mean(E)
-
-    # V-coordinate shift at full time step
-    for I in range(1, N - 1):
-        SGN = torch.sign(E[I])
-        SHIFT = abs(E[I]) * dt / dv
-        JJ = int(SHIFT)
-        SHIFT -= JJ
-
-        f_temp = torch.zeros(M)
-        if SGN > 0:
-            f_temp[0:JJ] = 0.0
-            f_temp[JJ:M] = f[I, 0:(M - JJ)]
-        else:
-            f_temp[0] = 0.0
-            f_temp[1:(M - JJ)] = f[I, (JJ + 1):M]
-            f_temp[(M - JJ):M] = 0.0
-
-        J = range(1, M - 1)
-        Dvf = torch.zeros(M)
-        Dvf[1:-1] = SHIFT * (f_temp[1:-1] + SGN * (f_temp[2:] - f_temp[:-2]) * (1.0 - SHIFT))
-
-    return f
+# def timestep(x,v,f_in,T,N,M,dt,dx,dv):
+#     f = torch.clone(f_in)
+#     # Plot EDF f(x,v,t) in phase space
+#     if T % 100 == -1:
+#         plt.pcolormesh(x[1:-1], v[1:-1], f[1:-1, 1:-1].T, shading='auto', cmap='hot')
+#         plt.axis('square')
+#         plt.clim(0, 0.0001)
+#         plt.colorbar()
+#         plt.title(f'Nonlinear Landau damping, t = {T * dt:.2f}', fontsize=18)
+#         plt.draw()
+#         plt.pause(0.01)
+#
+#     # X-coordinate shift at half time step
+#     for J in range(1, M - 1):
+#         SGN = torch.sign(v[J])
+#         SHIFT = torch.abs(v[J]) * 0.5 * dt / dx
+#         II = int(SHIFT)
+#         SHIFT -= II
+#
+#         # Make circular shift
+#         f_temp = roll(f[:, J], SGN.int() * II, 0)
+#         I = torch.arange(1, N - 1).to(f_temp.device)
+#         Dxf = torch.zeros(N).to(f_temp.device)
+#         Dxf[1:-1] = SHIFT * (f_temp[1:-1] + SGN * (f_temp[2:] - f_temp[:-2]) * (1.0 - SHIFT) / 4.0)
+#
+#         # Apply periodic border conditions for Dxf
+#         Dxf[-1] = Dxf[1]
+#         Dxf[0] = Dxf[-2]
+#
+#         # New distribution function after shift
+#         f[1:-1, J] = f_temp[1:-1] + Dxf[I - SGN.int()] - Dxf[I]
+#
+#     # Apply periodic boundaries in X-coordinate
+#     f[-1, :] = f[1, :]
+#     f[0, :] = f[-2, :]
+#
+#     # Electrical field strength from exact solution of Poisson's equation
+#     rho = torch.trapz(f, v, axis=1)
+#     E1 = torch.cumulative_trapezoid(rho, x)
+#     E1 = torch.cat((torch.zeros(1).to(E1.device), E1), dim=0)
+#     E = E1 - x
+#     E -= torch.mean(E)
+#
+#     # V-coordinate shift at full time step
+#     for I in range(1, N - 1):
+#         SGN = torch.sign(E[I])
+#         SHIFT = abs(E[I]) * dt / dv
+#         JJ = int(SHIFT)
+#         SHIFT -= JJ
+#
+#         f_temp = torch.zeros(M)
+#         if SGN > 0:
+#             f_temp[0:JJ] = 0.0
+#             f_temp[JJ:M] = f[I, 0:(M - JJ)]
+#         else:
+#             f_temp[0] = 0.0
+#             f_temp[1:(M - JJ)] = f[I, (JJ + 1):M]
+#             f_temp[(M - JJ):M] = 0.0
+#
+#         J = range(1, M - 1)
+#         Dvf = torch.zeros(M)
+#         Dvf[1:-1] = SHIFT * (f_temp[1:-1] + SGN * (f_temp[2:] - f_temp[:-2]) * (1.0 - SHIFT))
+#
+#     return f
+from time_module import timestep
 
 def loss(model,x,v,f):
     lf = 0.0
@@ -163,7 +164,17 @@ def Vlasov_Poisson_Landau_damping():
 
     while T <= N_steps:
         optimizer.zero_grad()
+
+        f_in  = torch.zeros_like(f)
+        for i,xi in enumerate(x):
+            for j,vi in enumerate(v):
+                xt = torch.cat((xi.reshape(1), vi.reshape(1)))
+                y = model(xt)
+                f_in[i][j] = y
+
         f1 = timestep(x,v,f,T,N,M,dt,dx,dv)
+        f_out = timestep(x, v, f_in, T, N, M, dt, dx, dv)
+        qq = 0
 
 
         if T % 10 == 0:
