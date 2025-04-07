5e802ea9fc4e8ad98814f0e458eb505ece7290f6
Alexey Snytnikov <snytav@gmail.com>
2025-04-07 23:13:23 +0200

    to 3D solve

--- torch_drucker.py
+++ torch_drucker.py
@@ -178,8 +178,8 @@ def Vlasov_Poisson_Landau_damping():
         f1 = timestep(x,v,f,T,N,M,dt,dx,dv)
         from pinn_pde_solve import pde_solve
         df = torch.subtract(f1,f)
-        df = df.reshape(1, df.shape[0], df.shape[1])
-        f_NN = pde_solve(df,f1,device,T*torch.ones(1),x,v,model)
+        # df = df.reshape(1, df.shape[0], df.shape[1])
+        # f_NN = pde_solve(df,f1,device,T*torch.ones(1),x,v,model)
 
         #f_out = timestep(x, v, f_in, T, N, M, dt, dx, dv)
         qq = 0
